58fb2e15a6eb3a048214f92f48d241a9815095d3
Justin Wilson <jkw0002@uah.edu>
2019-11-11 00:40:47 -0600

    Fixed shamt assign and pcsource assign, added changes to sub modules

--- mips_datapath.sv
+++ mips_datapath.sv
@@ -1,17 +1,39 @@
 /******************************************************************************** 
   File:           datapath.sv
-  Description:   	This module implements the datapath module of the MIPS processor,
+  Description:   	This module implements the datapath module of the MIPS processor.
+  
+						Inputs:
+						clk - Clock Signal
+						reset - Reset Signal
+						memdata - Data read from off processor memory
+						alusrca - Control Signal for ALUSRCA mux
+						alusrcb - Control Signal for ALUSRCB mux
+						pcen - Control Signal to allow update of PC register
+						pcsource - Control Signal for PCSOURCE mux
+						memtoreg - Control Signal for MEMDATAREG mu
+						regdst - Control Signal for REGDEST mux
+						iord - Control Signal for INSTR_DESTADDRESS mux
+						regwrite - Control Signal for enabling writes to register file
+						irwrite - Control Signal for enabling writes to Instruction register
+						alucontrol - Control Signal for MIPS ALU module
+						
+						Outputs:
+						addr - Address for reading to or writing from off processor memory
+						writedata - Data to be written to off processory memory
+						op - Instruction bits [31:26]
+						zero - Zero bit from ALU
+						funct - Instruction bits [5:0]
                   
   Date:           October 2019
   Author:        	Justin Wilson, jkw0002@uah.edu
 				
 *********************************************************************************/
 `timescale 1ns/1ps // Timescale for Test Bench, ignored during synthesis
-module mips_datapath (clk, reset, memdata, alusrca, alusrcb, pcen, pcsource, memtoreg, regdst, iord, regwrite, irwrite, alucontrol,
+module mips_datapath (clk, reset, memdata, alusrca, alusrcb, pcwrite, pcwriteCond, pcsource, memtoreg, regdst, iord, regwrite, irwrite, alucontrol,
 								addr, writedata, op, zero, funct);
 								
 	// Inputs
-	input logic clk, reset, alusrca, pcen, memtoreg, regdst, iord, regwrite, irwrite;
+	input logic clk, reset, alusrca, pcwrite, pcwriteCond, memtoreg, regdst, iord, regwrite, irwrite;
 	input logic [1:0] alusrcb, pcsource;
 	input logic [2:0] alucontrol;
 	input logic [31:0] memdata;
@@ -24,10 +46,11 @@ module mips_datapath (clk, reset, memdata, alusrca, alusrcb, pcen, pcsource, mem
 	// Internal Signals
 	logic [31:0] pcIn, pcOut, instruct, memOut, aluInA, aluInB, aluOut, Ain, Aout, Bin, Bout, wrData, aluResult;
 	logic [4:0] wrAddr, shamt;
-	logic pc_enable;
+	logic pc_enable, pcen;
 	
 	// PC Enable
-	assign pc_enable = pcen | zero;
+	assign pcen = pcwriteCond & zero;
+	assign pc_enable = pcwrite | pcen;
 	
 	// PC
 	register #(32) PC(clk, reset, pc_enable, pcIn, pcOut);
@@ -52,7 +75,7 @@ module mips_datapath (clk, reset, memdata, alusrca, alusrcb, pcen, pcsource, mem
 	assign wrData = (memtoreg) ? memOut : aluOut;
 	
 	// Shamt
-	assign shamt = instruct[4:0];
+	assign shamt = instruct[10:6];
 	
 	// ALU Input Muxes
 	assign aluInA = (alusrca) ? Aout : pcOut;
@@ -67,16 +90,16 @@ module mips_datapath (clk, reset, memdata, alusrca, alusrcb, pcen, pcsource, mem
 	register #(32) aluOutReg(clk, reset, 1'b1, aluResult, aluOut);
 	
 	// PC Source Mux
-	assign pcIn = (pcsource == 00) ? aluResult :
-						(pcsource == 01) ? aluOut :
-						(pcsource == 10) ? {6'd0,instruct[25:0]}: 32'd0;
+	assign pcIn = (pcsource == 2'b00) ? aluResult :
+						(pcsource == 2'b01) ? aluOut :
+						(pcsource == 2'b10) ? {6'd0,instruct[25:0]}: 32'd0;
 	
 	// Instruction Outputs
 	assign op = instruct[31:26];
 	assign funct = instruct[5:0];
 	
 	// Memory I/O
-	assign addr = (iord) ? pcOut : aluOut;
+	assign addr = (iord) ? aluOut : pcOut;
 	assign writedata = Bout;
 
 endmodule
