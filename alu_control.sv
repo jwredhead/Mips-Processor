0f8abdd7fed1cc83018eff548436e12aff8c5d69
Justin Wilson <jkw0002@uah.edu>
2019-11-11 00:37:39 -0600

    Added 1 bit to alucontrol and aluop for immed instr

--- alu_control.sv
+++ alu_control.sv
@@ -31,30 +31,34 @@ module alu_control (funct, aluop, alucontrol);
 
 	// Inputs
 	input logic [5:0] funct;
-	input logic [1:0] aluop;
+	input logic [2:0] aluop;
 	
 	// Outputs
-	output logic [2:0] alucontrol;
+	output logic [3:0] alucontrol;
 	
 	always_comb
 		case (aluop)
-			2'b00:	alucontrol = 3'b000;
-			2'b01:	alucontrol = 3'b001;
-			2'b10:	case(funct)
-							6'b000000: alucontrol = 3'b110;
-							6'b000010: alucontrol = 3'b111;
-							6'b100000: alucontrol = 3'b000;
-							6'b100001: alucontrol = 3'b000;
-							6'b100010: alucontrol = 3'b001;
-							6'b100011: alucontrol = 3'b001;
-							6'b100100: alucontrol = 3'b010;
-							6'b100101: alucontrol = 3'b011;
-							6'b100111: alucontrol = 3'b100;
-							6'b101010: alucontrol = 3'b101;
-							6'b101011: alucontrol = 3'b101;
-							default: alucontrol = 3'b000;
+			3'b000:	alucontrol = 4'b0000;
+			3'b001:	alucontrol = 4'b0001;
+			3'b010:	case(funct)
+							6'b000000: alucontrol = 4'b0110;
+							6'b000010: alucontrol = 4'b0111;
+							6'b100000: alucontrol = 4'b0000;
+							6'b100001: alucontrol = 4'b0000;
+							6'b100010: alucontrol = 4'b0001;
+							6'b100011: alucontrol = 4'b0001;
+							6'b100100: alucontrol = 4'b0010;
+							6'b100101: alucontrol = 4'b0011;
+							6'b100111: alucontrol = 4'b0100;
+							6'b101010: alucontrol = 4'b0101;
+							6'b101011: alucontrol = 4'b0101;
+							default: alucontrol = 4'b0000;
 						endcase
-			2'b11: 	alucontrol = 3'b000;
+			3'b011: 	alucontrol = 4'b1000;
+			3'b100:	alucontrol = 4'b0000;
+			3'b101:	alucontrol = 4'b0101;
+			3'b110:	alucontrol = 4'b0010;
+			3'b111:	alucontrol = 4'b0011;
 		endcase
 	
 endmodule
